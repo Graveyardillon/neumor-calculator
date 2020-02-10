0a98d6cf31f0181a78457e897d1f483ac2fa5df9
Papillon6814 <so1rou.k@gmail.com>
2020-02-10 22:36:26 +0900

    created a project

---
+++ NeumorCalculator/NeumorCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeumorCalculator
+//
+//  Created by Papillon on 2020/02/10.
+//  Copyright © 2020 Papillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
