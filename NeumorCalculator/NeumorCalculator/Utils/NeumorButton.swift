143facdfa2d53db011633b2fabba69ec296ac363
ohzw <52651644+ohzw@users.noreply.github.com>
2020-02-12 21:19:00 +0900

    NeumorButtonっぽいもの

--- NeumorCalculator/NeumorCalculator/Utils/NeumorButton.swift
+++ NeumorCalculator/NeumorCalculator/Utils/NeumorButton.swift
@@ -22,55 +22,42 @@ class NeumorButton: UIButton {
     let buttonLayer = CALayer(), layer2 = CALayer(), layer3 = CALayer()
     
     func button() {
-//        shadow()
-//        highlight()
-
-        buttonLayer.backgroundColor = UIColor(red: 242, green: 242, blue: 242, alpha: 1).cgColor
-        buttonLayer.cornerRadius = 15
+        setTitle("BUTTON" , for: .normal)
+        setTitleColor(UIColor.gray, for: .normal)
+        
+        buttonLayer.backgroundColor = UIColor(red: 231/255, green: 231/255, blue: 231/255, alpha: 1).cgColor
+        buttonLayer.cornerRadius = layer.frame.size.height * 0.15
         [buttonLayer, layer2, layer3].forEach {
             $0.masksToBounds = false
             $0.frame = layer.bounds
             layer.insertSublayer($0, at: 0)
         }
-        layer2.applySketchShadow(color: UIColor.red, alpha: 0.5, x: 6, y: 6, blur: 20, spread: -10)
-        layer3.applySketchShadow(color: UIColor.green, alpha: 0.5, x: -6, y: -6, blur: 20, spread: -9)
-    }
-    
-    private func shadow() {
-        layer.shadowOpacity = 0.2
-        layer.shadowRadius = 10
-        layer.shadowOffset = CGSize(width: 6, height: 6)
-        layer.shadowColor = UIColor.black.cgColor
-    }
-    private func highlight() {
-        layer.shadowOpacity = 1
-        layer.shadowRadius = 10
-        layer.shadowOffset = CGSize(width: -6, height: -6)
-        layer.shadowColor = UIColor.red.cgColor
+        layer2.applySketchShadow(color: UIColor.white, alpha: 1, x: -3, y: -3, blur: 10, spread: -9)
+        layer3.applySketchShadow(color: UIColor.black, alpha: 1, x: 3, y: 3, blur: 10, spread: 1)
+
     }
 }
 
-
 extension CALayer {
     func applySketchShadow(
-        color: UIColor = .black,
-        alpha: Float = 0.5,
-        x: CGFloat = 0,
-        y: CGFloat = 2,
-        blur: CGFloat = 4,
-        spread: CGFloat = 0)
+        color: UIColor,
+        alpha: Float,
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat,
+        spread: CGFloat)
     {
         shadowColor = color.cgColor
         shadowOpacity = alpha
         shadowOffset = CGSize(width: x, height: y)
-        shadowRadius = blur / 2.0
-        if spread == 0 {
-            shadowPath = nil
-        } else {
-            let dx = -spread
-            let rect = bounds.insetBy(dx: dx, dy: dx)
-            shadowPath = UIBezierPath(rect: rect).cgPath
-        }
+        shadowRadius = blur
+            if spread == 0 {
+                shadowPath = nil
+            } else {
+                let dx = spread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                shadowPath = UIBezierPath(rect: rect).cgPath
+            }
         masksToBounds = false
     }
 }
